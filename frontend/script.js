3e998b15a21255c8e4a735e560f8007df55a3f75
Tathya <coding.tathya@gmail.com>
2025-03-16 14:05:59 +0530

    feat: Implemented alliegance switching

--- frontend/script.js
+++ frontend/script.js
@@ -18,7 +18,17 @@ const pieceMap = {
   'p': 'pawn'
 }
 
-let turn = 'w';
+let visibilityMask = [
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1]
+]
+
 let selectedPiece = null;
 
 function populateBoard() {
@@ -49,12 +59,73 @@ function placePieces() {
   }
 }
 
+function updatePieces() {
+  var board = document.getElementById('board');
+  var squares = board.getElementsByClassName('cell');
+  for (let i = 0; i < 64; i++) {
+    if (squares[i].getElementsByClassName('piece').length === 0 && position[Math.floor(i / 8)][i % 8] !== ' ') {
+      var piece = document.createElement('img');
+      const pieceText = position[Math.floor(i / 8)][i % 8];
+      const pieceIsDark = pieceText.toLowerCase() === pieceText;
+      piece.src = `assets/${pieceIsDark ? 'black' : 'white'}-${pieceMap[pieceText.toLowerCase()]}.png`;
+      piece.className = 'piece';
+      squares[i].appendChild(piece);
+    }
+    else if (squares[i].getElementsByClassName('piece').length > 0 && position[Math.floor(i / 8)][i % 8] === ' ') {
+      squares[i].removeChild(squares[i].getElementsByClassName('piece')[0]);
+    } else if (squares[i].getElementsByClassName('piece').length > 0) {
+      const piece = squares[i].getElementsByClassName('piece')[0];
+      const pieceText = position[Math.floor(i / 8)][i % 8];
+      const pieceIsDark = pieceText.toLowerCase() === pieceText;
+      piece.src = `assets/${pieceIsDark ? 'black' : 'white'}-${pieceMap[pieceText.toLowerCase()]}.png`;
+    }
+  }
+}
+
+function updateVisibility() {
+  var board = document.getElementById('board');
+  var squares = board.getElementsByClassName('cell');
+  for (let i = 0; i < 64; i++) {
+    if (squares[i].getElementsByClassName('piece').length === 0) {
+      continue;
+    } 
+    let piece = squares[i].getElementsByClassName('piece')[0];
+    
+    piece.setAttribute('style', `visibility: ${visibilityMask[Math.floor(i / 8)][i % 8] === 1 ? 'visible' : 'hidden'}`);
+  }
+}
+
+function determineAllegienceSwitch() {
+  if (Math.random() < 1.25) {
+    let whitePieceIndices = [];
+    for (let i = 0; i < 64; i++) {
+      if (position[Math.floor(i / 8)][i % 8] !== ' ' && position[Math.floor(i / 8)][i % 8].toLowerCase() !== position[Math.floor(i / 8)][i % 8]) {
+        whitePieceIndices.push(i);
+      }
+    }
+
+    let randomIndex = whitePieceIndices[Math.floor(Math.random() * whitePieceIndices.length)];
+    let piece = document.getElementsByClassName('cell')[randomIndex].getElementsByClassName('piece')[0];
+    let pieceText = position[Math.floor(randomIndex / 8)][randomIndex % 8];
+
+    position[Math.floor(randomIndex / 8)][randomIndex % 8] = pieceText.toLowerCase();
+    updatePieces();
+  }
+}
+
 function squareClicked() {
   const square = this;
   if (selectedPiece === null) {
     if (square.getElementsByClassName('piece').length === 0) {
       return;
     }
+
+    const piece = square.getElementsByClassName('piece')[0];
+    const pieceIsDark = piece.src.includes('black');
+    if (pieceIsDark) {
+      return;
+    }
+
     selectedPiece = square;
     square.classList.add('selected');
   }
@@ -66,12 +137,21 @@ function squareClicked() {
     }
 
     if (isValidMove(square, selectedPiece)) {
+      // Update position array
       const piece = selectedPiece.getElementsByClassName('piece')[0];
-      square.innerHTML = '';
-      square.appendChild(piece);
-      selectedPiece.innerHTML = '';
+      const pieceText = piece.src.split('/').pop().split('.')[0].split('-')[1];
+      const pieceRow = Math.floor(Array.from(selectedPiece.parentNode.children).indexOf(selectedPiece) / 8);
+      const pieceCol = Array.from(selectedPiece.parentNode.children).indexOf(selectedPiece) % 8;
+      const squareRow = Math.floor(Array.from(square.parentNode.children).indexOf(square) / 8);
+      const squareCol = Array.from(square.parentNode.children).indexOf(square) % 8;
+
+      position[squareRow][squareCol] = position[pieceRow][pieceCol];
+      position[pieceRow][pieceCol] = ' ';
+
       selectedPiece.classList.remove('selected');
       selectedPiece = null;
+
+      updatePieces();
 
       fetch('api/move/', {
         method: 'POST',
@@ -82,6 +162,11 @@ function squareClicked() {
           position: position,
         })
       })
+
+      // Randomize Visibility mask
+      visibilityMask = visibilityMask.map(row => row.map(() => Math.random() < 0.5 ? 0 : 1));
+      updateVisibility();
+      determineAllegienceSwitch();
     }
   }
 }
