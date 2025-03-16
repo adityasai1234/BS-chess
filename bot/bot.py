1900e6ea1b457d27873753a6d8434b23128dce78
Tathya <coding.tathya@gmail.com>
2025-03-16 18:28:55 +0530

    chore: Removed unnecessary numpy dependency

--- bot/bot.py
+++ bot/bot.py
@@ -1,10 +1,10 @@
 import chess
-import numpy as np
+import random
 
 def get_ai_move(board):
     legal_moves = list(board.legal_moves)
     if legal_moves:
-        move = np.random.choice(legal_moves)
+        move = random.choice(legal_moves)
         return f"{chess.square_name(move.from_square)}-{chess.square_name(move.to_square)}"
     
     return "resign"
